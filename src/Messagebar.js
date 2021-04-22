141e55624b30c98ea2d11cc9242ccc54c4e868cd
Tyler Knapp <knapptr@gmail.com>
2021-04-22 13:13:50 -0700

    removes dead files

--- src/Messagebar.js
+++
@@ -1,8 +0,0 @@
-import { MessageWrapper, Text } from "./styles/components";
-export const MessageBar = ({ message }) => {
-    return (
-        <MessageWrapper>
-            <Text>{message}</Text>
-        </MessageWrapper>
-    );
-};
